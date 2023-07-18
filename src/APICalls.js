cad994c68c448d44d7cb52c15a7a2e1305020f0f
Doris Lin <lmh7026@gmail.com>
2023-07-18 13:43:30 -0400

    create func getSelectedBoard

--- src/APICalls.js
+++ src/APICalls.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+const kBaseUrl = 'https://inspiration-board-backend-t6x0.onrender.com/boards';
+
 /**
  * getAllBoards takes no arguments.
  * Returns an array of boards
@@ -7,12 +9,25 @@ import axios from 'axios';
  */
 export const getAllBoards = () => {
     return axios
-    .get('https://inspiration-board-backend-t6x0.onrender.com/boards')
+    .get(`${kBaseUrl}`)
     .then(response => {
         const allBoards = response.data
-        console.log(allBoards)
         return allBoards
-    })
+    });
 };
 
+/**
+ * getSelectedBoard takes one argument, an int representing an id.
+ * Returns a board
+ * @param {Int} id must be an integer
+ * @returns {Promise<Board>} Promise object represents a Board object
+ */
+export const getSelectedBoard = (id) => {
+    return axios
+    .get(`${kBaseUrl}/${id}`)
+    .then(response => {
+        const selectedBoard = response.data
+        return selectedBoard
+    });
+};
 
