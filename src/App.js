dfbe0fc45d8779fa00b71b26c7d954b37358b0b2
anclark686 <anclark686@gmail.com>
2023-07-18 19:19:11 -0400

    adding newCardForm prototype

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
 
+import NewCardForm from './components/NewCardForm';
+
 const App = () => {
   return (
     <div className="App">
       <h1>Hello World</h1>
+
+      <section className="NewCardForm__container">
+        <NewCardForm boardId={boardId} createNewBoard={backend.createNewBoard}/>
+      </section>
     </div>
   );
 }
