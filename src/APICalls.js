8af561b9f63e9be48bd2756f6c63be3534bec0a6
Doris Lin <lmh7026@gmail.com>
2023-07-18 14:14:39 -0400

    create funcs createNewBoard, createNewCard, getBoardCards

--- src/APICalls.js
+++ src/APICalls.js
@@ -17,9 +17,9 @@ export const getAllBoards = () => {
 };
 
 /**
- * getSelectedBoard takes one argument, an int representing an id.
+ * getSelectedBoard takes one argument, an int representing a board id.
  * Returns a board
- * @param {Int} id must be an integer
+ * @param {int} id must be an integer
  * @returns {Promise<Board>} Promise object represents a Board object
  */
 export const getSelectedBoard = (id) => {
@@ -31,3 +31,55 @@ export const getSelectedBoard = (id) => {
     });
 };
 
+/**
+ * createNewBoard takes one argument, an object containing data needed to create a new Board object .
+ * Returns the newly created board
+ * @param {Object} data
+ * @param {String} data.title
+ * @param {String} data.owner
+ * @returns {Promise<Board>} Promise object represents a Board object
+ */
+export const createNewBoard = (data) => {
+    axios
+    .post(kBaseUrl, data)
+    .then(response => {
+        const newBoard = response.data
+        return newBoard
+    });
+};
+
+/**
+ * createNewCard takes one argument, an object containing data needed to create a new Card object .
+ * Returns the newly created card
+ * @param {Object} data
+ * @param {String} data.message
+ * @param {int} data.likes_count
+ * @param {int} data.board_id
+ * @returns {Promise<Card>} Promise object represents a Card object
+ */
+export const createNewCard = (data) => {
+    axios
+    .post(`${kBaseUrl}/${data.board_id}/cards`, data)
+    .then(response => {
+        console.log(response.data)
+        const newCard = response.data
+        return newCard
+    })
+}
+
+
+/**
+ * getBoardCards takes one argument, an int representing a board id
+ * Returns an array of cards associated with that board_id
+ * @param {int} id must be an integer
+ * @returns {Promise<Card[]>} Promise object represents an array of Card objects
+ */
+export const getBoardCards = (id) => {
+    return axios
+    .get(`${kBaseUrl}/${id}/cards`)
+    .then(response => {
+        const allCards = response.data
+        console.log(allCards)
+        return allCards
+    })
+}
