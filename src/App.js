5191d3c5e8874f9183cd227f5fa35b0da461b5ca
Doris Lin <lmh7026@gmail.com>
2023-07-19 11:35:30 -0400

    implement NewBoardForm component with form submission handling

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
+import { useState } from "react";
 import './App.css';
 // To use API call functions, use apiCalls as the module name, e.g., backend.getAllBoards() will return a promise of an array of Board objects. 
 import * as backend from './APICalls.js'
 
 
 import NewCardForm from './components/NewCardForm';
+import NewBoardForm from './components/NewBoardForm';
+const kBoardBaseUrl = 'https://inspiration-board-backend-t6x0.onrender.com/boards';
 
 const App = () => {
+  const [boardData, setBoardData] = useState([]);
+
+  const handleNewBoardSubmit = (data) => {
+    backend.createNewBoard(data)
+    .then(result => {
+      setBoardData(prev => [result, ...prev]);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  };
+  
   return (
     <div className="App">
       <h1>Hello World</h1>
-
+      <section className="newBoardForm__container">
+        <NewBoardForm handleNewBoardSubmit={handleNewBoardSubmit}/>
+      </section>
       <section className="NewCardForm__container">
-        <NewCardForm boardId={boardId} createNewBoard={backend.createNewBoard}/>
+        <NewCardForm boardId={1} createNewBoard={backend.createNewBoard}/>
       </section>
     </div>
   );
--- src/components/NewBoardForm.js
+++ src/components/NewBoardForm.js
@@ -1,9 +1,58 @@
+import { useState } from "react";
+import PropTypes from 'prop-types';
+
 import './NewBoardForm.css';
 
-const NewBoardForm = () => {
+
+const kInitialFormData = {
+  title: '',
+  owner: '',
+};
+
+const NewBoardForm = ({handleNewBoardSubmit}) => {
+  const [formData, setFormData] = useState(kInitialFormData);
+
+  const handleChange = (event) => {
+    const value = event.target.value;
+    const name = event.target.name;
+    setFormData(prev => ({
+      ...prev, [name]: value
+    }));
+  };
+  
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    // API call to create a new board in db
+    handleNewBoardSubmit(formData);
+    // clear input field
+    setFormData(kInitialFormData);
+  };
+
   return (
     <div className="NewBoardForm">
-      <h1>NewBoardForm</h1>
+      <h1>Create a New Board</h1>
+      <form className="new-board-form" onSubmit={handleFormSubmit}>
+        <li>
+          <label htmlFor="title">Title: </label>
+          <input 
+            type="text" 
+            name="title" 
+            value={formData.title} 
+            onChange={handleChange} 
+          />
+        </li>
+        <li>
+          <label htmlFor="owner">Owner: </label>
+          <input 
+            type="text" 
+            name="owner" 
+            value={formData.owner} 
+            onChange={handleChange} 
+          />
+        </li>
+        <p id="preview">Preview: {formData.title} - {formData.owner}</p>
+        <input type="submit" className="submit-btn" />
+      </form>
     </div>
   );
 }
