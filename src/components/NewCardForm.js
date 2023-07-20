1936a498b7cb88bd6c3ab0d370609a25885520c9
anclark686 <anclark686@gmail.com>
2023-07-19 23:28:28 -0400

    removing stray console logs, adding some missing semicolons

--- src/components/NewCardForm.js
+++ src/components/NewCardForm.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
 import "./NewCardForm.css";
 
@@ -8,28 +8,25 @@ const NewCardForm = ({ boardId, createNewBoard }) => {
   const [invalidForm, setInvalidForm] = useState(true);
 
   const generateMessage = (event) => {
-    setMessage(event.target.value)
+    setMessage(event.target.value);
     if (event.target.value.length > 0 && event.target.value.length <= 40) {
       setInvalidForm(false);
     } else {
       setInvalidForm(true);
     }
-  }
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(message);
-    console.log(boardId)
     const data = {
       message: message,
       board_id: boardId,
       likes_count: 0,
     };
-    console.log(data);
     createNewBoard(data);
     setMessage("");
     setInvalidForm(true);
-  }
+  };
 
   return (
     <section>
@@ -37,20 +34,27 @@ const NewCardForm = ({ boardId, createNewBoard }) => {
 
       <form className="new-card__form" onSubmit={handleSubmit}>
         <label htmlFor="message">Message: </label>
-        <input type="text" className={invalidForm ? "invalid-form-input" : "form-input"} name="message" value={message} onChange={generateMessage} />
+        <input
+          type="text"
+          className={invalidForm ? "invalid-form-input" : "form-input"}
+          name="message"
+          value={message}
+          onChange={generateMessage}
+        />
         <p id="preview">Preview: {message}</p>
-        {!invalidForm ?
+        {!invalidForm ? (
           <input type="Submit" className="submit-btn" />
-          :
-          <input type="Submit" className="submit-btn" disabled />}
+        ) : (
+          <input type="Submit" className="submit-btn" disabled />
+        )}
       </form>
     </section>
   );
-}
+};
 
 NewCardForm.propTypes = {
   boardId: PropTypes.number.isRequired,
   createNewBoard: PropTypes.func.isRequired,
-}
+};
 
 export default NewCardForm;
