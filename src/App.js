b910fb575c43d42334c017c1a8ccf019ccbdf9d8
anclark686 <anclark686@gmail.com>
2023-07-19 20:46:25 -0400

    adding css import back

--- src/App.js
+++ src/App.js
@@ -1,29 +1,35 @@
 import React, { useState, useEffect } from "react";
-import * as backend from './APICalls.js';
+
+import * as backend from "./APICalls.js";
+
 import BoardList from "./components/BoardList";
-import NewCardForm from './components/NewCardForm';
-import NewBoardForm from './components/NewBoardForm';
+import NewCardForm from "./components/NewCardForm";
+import NewBoardForm from "./components/NewBoardForm";
+
+import "./App.css";
 
 const App = () => {
   const [boardData, setBoardData] = useState([]);
   const [selectedBoardId, setSelectedBoardId] = useState(null);
 
   useEffect(() => {
-    backend.getAllBoards()
-      .then(boards => {
+    backend
+      .getAllBoards()
+      .then((boards) => {
         setBoardData(boards);
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err);
       });
   }, []);
 
   const handleNewBoardSubmit = (data) => {
-    backend.createNewBoard(data)
-      .then(result => {
-        setBoardData(prevBoardData => [result, ...prevBoardData]);
+    backend
+      .createNewBoard(data)
+      .then((result) => {
+        setBoardData((prevBoardData) => [result, ...prevBoardData]);
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err);
       });
   };
@@ -37,9 +43,11 @@ const App = () => {
       <div className="content__container">
         <h1>Inspiration Board</h1>
         <section className="boards__container">
-          
           <section className="BoardList__container">
-            <BoardList boardData={boardData} onBoardSelect={handleBoardSelect} />
+            <BoardList
+              boardData={boardData}
+              onBoardSelect={handleBoardSelect}
+            />
           </section>
 
           <section className="newBoardForm__container">
@@ -48,11 +56,13 @@ const App = () => {
         </section>
         <section className="cards_container">
           <section className="NewCardForm__container">
-            <NewCardForm boardId={selectedBoardId} createNewBoard={backend.createNewBoard} />
+            <NewCardForm
+              boardId={selectedBoardId}
+              createNewBoard={backend.createNewBoard}
+            />
           </section>
         </section>
       </div>
-
     </div>
   );
 };
